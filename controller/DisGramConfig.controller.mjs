75e6896ab788b51af035115b2a54db88e90facdf
OmegaPirat <OmegaPirat>
2022-08-07 22:49:16 +0200

    Add retention time

--- controller/DisGramConfig.controller.mjs
+++ controller/DisGramConfig.controller.mjs
@@ -2,38 +2,51 @@ import express from 'express';
 import { Router } from 'express';
 import { Client, GatewayIntentBits } from 'discord.js';
 import { Telegraf } from 'telegraf';
+import db from '../db/index.mjs';
 
 const router = Router();
-let telegramBot;
-let discordBot;
+let telegramClient;
+let discordClient;
+
 
 router.use(express.json());
 
 router.get('/enable', async (req, res) => {
-   discordBot?.destroy();
+   let telegramBot = await db.TelegramBot.findOne({
+      include: db.TelegramChannel
+   });
+   let discordBot = await db.DiscordBot.findOne({
+      include: db.DiscordChannel
+   });
+
+   discordClient?.destroy();
    console.log('DisGram bot is destroyed.')
 
 
-   telegramBot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
-   res.status(401);
+   telegramClient = new Telegraf(telegramBot.token);
+   discordClient = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-   discordBot = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-
-   discordBot.once('ready', () => {
+   discordClient.once('ready', () => {
       console.log('DisGram bot is ready!');
 
    });
 
-   discordBot.on("messageCreate", (message) => {
-      if (message.channel.name === process.env.discordChannelName) {
-         telegramBot.telegram.sendMessage(process.env.telegramChannelId, message.content);
+   discordClient.on("messageCreate", (message) => {
+      if (message.channel.name === discordBot.DiscordChannel.name) {
+         telegramBot.TelegramChannels.forEach(async it => {
+            let telegramMessage = await telegramClient.telegram.sendMessage(it.channelId, message.content);
+            if (it.retentionTime) {
+               setTimeout(() => {
+                  telegramClient.telegram.deleteMessage(it.channelId, telegramMessage.message_id);
+               }, 1000 * 60 * it.retentionTime);
+            }
+         });
       }
-
    });
 
    try {
-      await discordBot.login(process.env.DISCORD_BOT_TOKEN);
-      await telegramBot.launch();
+      await discordClient.login(discordBot.token);
+      await telegramClient.launch();
    }
    catch (error) {
       res.status(401);
--- index.mjs
+++ index.mjs
@@ -5,6 +5,7 @@ import discordBotRouter from './controller/DiscordBot.controller.mjs'
 import discordChannelRouter from './controller/DiscordChannel.controller.mjs';
 import telegramBotRouter from './controller/TelegramBot.controller.mjs';
 import telegramChannelRouter from './controller/TelegramChannel.controller.mjs';
+import disGramConfigRouter from './controller/DisGramConfig.controller.mjs';
 import cors from 'cors';
 import db from './db/index.mjs';
 
@@ -16,6 +17,7 @@ app.use('/api/v1/DiscordBot', discordBotRouter);
 app.use('/api/v1/DiscordChannel', discordChannelRouter);
 app.use('/api/v1/TelegramBot', telegramBotRouter);
 app.use('/api/v1/TelegramChannel', telegramChannelRouter);
+app.use('/api/v1/config', disGramConfigRouter);
 
 app.listen(process.env.PORT || 5000);
 
