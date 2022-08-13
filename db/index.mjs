d1b52553e3befa481d2c3a69afe299fc924f3891
OmegaPirat <co9516514@gmail.com>
2022-08-13 15:47:13 +0200

    Enable ssl

--- db/index.mjs
+++ db/index.mjs
@@ -11,7 +11,7 @@ import Sequelize from "sequelize";
 //     dialect: dbConfig.DIALECT
 // });
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {} || {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
         ssl: {
             require: true,
